06de979a77c1798c7447d9b9ee4a7e1bf6197a2b
joseluan <joseluan3301@gmail.com>
2025-11-10 19:38:51 -0300

    Adicionando testes de integracao

--- db/engine.py
+++ db/engine.py
@@ -20,20 +20,3 @@ def get_mongo_collection(collection_name: str):
 if __name__ == "__main__":
     ENV = os.getenv("ENV", "prod").lower()
     collection = get_mongo_collection(f"{ENV.upper()}_intent_logs")
-    import requests
-    from datetime import datetime
-
-    text = "Texto de teste salvar no mongo"
-    response = requests.post(f"http://localhost:8000/predict?text={text}")
-    print(response.text)    
-    payload = response.json()
-    data_teste = datetime.fromtimestamp(payload['timestamp'])
-    ENV = os.getenv("ENV", "prod").lower()
-    collection = get_mongo_collection(f"{ENV.upper()}_intent_logs")
-    print('data_teste', data_teste)
-    filtro = {
-        "timestamp": payload['timestamp'],
-        "text": text
-    }
-    documento_encontrado = collection.find_one(filtro)
-    print(documento_encontrado)
